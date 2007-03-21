c03285c4051c655b1c674f4ff1309c3bc9c90c83
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2007-03-21 17:29:45 +0000

    obsolete
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@82 ac951b51-2923-0410-bde8-533199751630

--- pycompat.h
+++
@@ -1,7 +0,0 @@
-#include "Python.h"
-
-/* Suggested by M. v. Loewis: http://mail.python.org/pipermail/python-dev/2006-March/062561.html */
-
-#if (PY_VERSION_HEX < 0x02050000)
-typedef int Py_ssize_t;
-#endif
