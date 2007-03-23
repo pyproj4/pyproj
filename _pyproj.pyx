a41c1d188938ba69d1f657ba8296bb5eace44479
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2007-03-23 21:38:59 +0000

    geod support updates
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@97 ac951b51-2923-0410-bde8-533199751630

--- _pyproj.pyx
+++ _pyproj.pyx
@@ -27,6 +27,7 @@ cdef extern from "geodesic.h":
         int merid, signS
     GEODESIC_T *GEOD_init_plus(char *args, GEODESIC_T *g)
     void geod_for(GEODESIC_T *g)
+    void geod_pre(GEODESIC_T *g)
     int geod_inv(GEODESIC_T *g)
 
 cdef extern from "proj_api.h":
@@ -217,12 +218,13 @@ def _transform(Proj p1, Proj p2, inx, iny, inz, radians):
             yy[i] = yy[i]*_rad2dg
 
 cdef class Geod:
-    cdef GEODESIC_T GEOD_T
+    cdef GEODESIC_T geodesic_t
     cdef public object geodparams
     cdef object proj_version
     cdef char *geodinitstring
 
     def __new__(self, geodparams):
+        cdef GEODESIC_T GEOD_T
         self.geodparams = geodparams
         # setup proj initialization string.
         geodargs = []
@@ -230,7 +232,7 @@ cdef class Geod:
             geodargs.append('+'+key+"="+str(value)+' ')
         self.geodinitstring = PyString_AsString(''.join(geodargs))
         # initialize projection
-        self.geodesic_t = geod_init_plus(self.geodinitstring, GEOD_T)
+        self.geodesic_t = GEOD_init_plus(self.geodinitstring, &GEOD_T)[0]
         if pj_errno != 0:
             raise RuntimeError(pj_strerrno(pj_errno))
         self.proj_version = PJ_VERSION/100.
@@ -238,3 +240,107 @@ cdef class Geod:
     def __reduce__(self):
         """special method that allows pyproj.Geod instance to be pickled"""
         return (self.__class__,(self.projparams,))
+
+    def _fwd(self, object lons, object lats, object az, object dist, radians=False):
+        """
+ forward transformation - determine longitude, latitude and back azimuth 
+ of a terminus point given an initial point longitude and latitude, plus
+ forward azimuth and distance.
+ if radians=True, lons/lats are radians instead of degrees.
+        """
+        cdef projUV p1, p2
+        cdef Py_ssize_t buflenlons, buflenlats, buflenaz, buflend
+        cdef int ndim, i
+        cdef double *lonsdata, *latsdata, *azdata, *distdata
+        cdef void *londata, *latdata, *azdat, *distdat
+        # if buffer api is supported, get pointer to data buffers.
+        if PyObject_AsWriteBuffer(lons, &londata, &buflenlons) <> 0:
+            raise RuntimeError
+        if PyObject_AsWriteBuffer(lats, &latdata, &buflenlats) <> 0:
+            raise RuntimeError
+        if PyObject_AsWriteBuffer(az, &azdat, &buflenaz) <> 0:
+            raise RuntimeError
+        if PyObject_AsWriteBuffer(dist, &distdat, &buflend) <> 0:
+            raise RuntimeError
+        # process data in buffer
+        if not buflenlons == buflenlats == buflenaz == buflend:
+            raise RuntimeError("Buffer lengths not the same")
+        ndim = buflenlons/_doublesize
+        lonsdata = <double *>londata
+        latsdata = <double *>latdata
+        azdata = <double *>azdat
+        distdata = <double *>distdat
+        for i from 0 <= i < ndim:
+            if radians:
+                self.geodesic_t.p1.u = lonsdata[i]
+                self.geodesic_t.p1.v = latsdata[i]
+                self.geodesic_t.ALPHA12 = azdata[i]
+                self.geodesic_t.DIST = distdata[i]
+            else:
+                self.geodesic_t.p1.u = _dg2rad*lonsdata[i]
+                self.geodesic_t.p1.v = _dg2rad*latsdata[i]
+                self.geodesic_t.ALPHA12 = _dg2rad*azdata[i]
+                self.geodesic_t.DIST = distdata[i]
+            geod_pre(&self.geodesic_t)
+            if pj_errno != 0:
+                raise RuntimeError(pj_strerrno(pj_errno))
+            geod_for(&self.geodesic_t)
+            if pj_errno != 0:
+                raise RuntimeError(pj_strerrno(pj_errno))
+            if radians:
+                lonsdata[i] = self.geodesic_t.p2.u
+                latsdata[i] = self.geodesic_t.p2.v
+                azdata[i] = self.geodesic_t.ALPHA21
+            else:
+                lonsdata[i] = _rad2dg*self.geodesic_t.p2.u
+                latsdata[i] = _rad2dg*self.geodesic_t.p2.v
+                azdata[i] = _rad2dg*self.geodesic_t.ALPHA21
+
+    def _inv(self, object lons1, object lats1, object lons2, object lats2, radians=False):
+        """
+ inverse transformation - return forward and back azimuths, plus distance
+ between an initial and terminus lat/lon pair.
+ if radians=True, lons/lats are radians instead of degrees.
+        """
+        cdef Py_ssize_t buflenlons, buflenlats, buflenaz, buflend
+        cdef int ndim, i
+        cdef double *lonsdata, *latsdata, *azdata, *distdata
+        cdef void *londata, *latdata, *azdat, *distdat
+        # if buffer api is supported, get pointer to data buffers.
+        if PyObject_AsWriteBuffer(lons1, &londata, &buflenlons) <> 0:
+            raise RuntimeError
+        if PyObject_AsWriteBuffer(lats1, &latdata, &buflenlats) <> 0:
+            raise RuntimeError
+        if PyObject_AsWriteBuffer(lons2, &azdat, &buflenaz) <> 0:
+            raise RuntimeError
+        if PyObject_AsWriteBuffer(lats2, &distdat, &buflend) <> 0:
+            raise RuntimeError
+        # process data in buffer
+        if not buflenlons == buflenlats == buflenaz == buflend:
+            raise RuntimeError("Buffer lengths not the same")
+        ndim = buflenlons/_doublesize
+        lonsdata = <double *>londata
+        latsdata = <double *>latdata
+        azdata = <double *>azdat
+        distdata = <double *>distdat
+        for i from 0 <= i < ndim:
+            if radians:
+                self.geodesic_t.p1.v = lonsdata[i]
+                self.geodesic_t.p1.u = latsdata[i]
+                self.geodesic_t.p2.v = azdata[i]
+                self.geodesic_t.p2.u = distdata[i]
+            else:
+                self.geodesic_t.p1.v = _dg2rad*lonsdata[i]
+                self.geodesic_t.p1.u = _dg2rad*latsdata[i]
+                self.geodesic_t.p2.v = _dg2rad*azdata[i]
+                self.geodesic_t.p2.u = _dg2rad*distdata[i]
+            geod_inv(&self.geodesic_t)
+            if pj_errno != 0:
+                raise RuntimeError(pj_strerrno(pj_errno))
+            if radians:
+                lonsdata[i] = self.geodesic_t.ALPHA12
+                latsdata[i] = self.geodesic_t.ALPHA21
+            else:
+                lonsdata[i] = _rad2dg*self.geodesic_t.ALPHA12
+                latsdata[i] = _rad2dg*self.geodesic_t.ALPHA21
+            azdata[i] = self.geodesic_t.DIST
