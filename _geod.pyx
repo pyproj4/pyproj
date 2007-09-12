6bac2fc57e5ccb1c564f42264c9269e7661da8ee
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2007-09-12 18:13:37 +0000

    fix isnan check
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@143 ac951b51-2923-0410-bde8-533199751630

--- _geod.pyx
+++ _geod.pyx
@@ -70,7 +70,7 @@ cdef class Geod:
             geod_for(&self.geodesic_t)
             if pj_errno != 0:
                 raise RuntimeError(pj_strerrno(pj_errno))
-            if isnan(self.geodesic_t.ALPHA21) == FP_NAN:
+            if isnan(self.geodesic_t.ALPHA21):
                 raise ValueError('undefined forward geodesic (may be an equatorial arc)')
             if radians:
                 lonsdata[i] = self.geodesic_t.p2.v
@@ -119,7 +119,7 @@ cdef class Geod:
                 self.geodesic_t.p2.v = _dg2rad*azdata[i]
                 self.geodesic_t.p2.u = _dg2rad*distdata[i]
             geod_inv(&self.geodesic_t)
-            if isnan(self.geodesic_t.DIST) == FP_NAN:
+            if isnan(self.geodesic_t.DIST):
                 raise ValueError('undefined inverse geodesic (may be an antipodal point)')
             if pj_errno != 0:
                 raise RuntimeError(pj_strerrno(pj_errno))
