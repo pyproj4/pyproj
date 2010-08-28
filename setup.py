8a26a530fffa81adae66d400b8e1e5dd41e8bdde
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2010-08-28 18:43:30 +0000

    change description
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@195 ac951b51-2923-0410-bde8-533199751630

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ package_data = {'pyproj':datafiles}
 
 setup(name = "pyproj",
   version = "1.8.8",
-  description = "Pyrex generated python interface to PROJ.4 library",
+  description = "Python interface to PROJ.4 library",
   long_description  = """
 Performs cartographic transformations between geographic (lat/lon)
 and map projection (x/y) coordinates. Can also transform directly
