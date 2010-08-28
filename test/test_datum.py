6e9d83ed5387ab279b39c56c9b10245f242dd8fc
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2010-08-28 04:11:51 +0000

    fix test
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@182 ac951b51-2923-0410-bde8-533199751630

--- test/test_datum.py
+++ test/test_datum.py
@@ -4,8 +4,10 @@ p1 = Proj(proj='latlong',datum='WGS84')
 s_1 = -111.5
 s_2 = 45.25919444444
 p2 = Proj(proj="utm",zone=10,datum='NAD27')
-#cstr='echo "-111.5 45.25919444444" | cs2cs +proj=latlong +to '+\
-cstr='echo "-111.5 45.25919444444" | /opt/local/bin/cs2cs -v +proj=latlong +datum=WGS84 +to '+\
+cstr='echo "-111.5 45.25919444444" | cs2cs +proj=latlong +datum=WGS84 +to '+\
      '+proj=utm +zone=10 +datum=NAD27 -f "%.12f"'
-print commands.getoutput(cstr)
-print transform(p1, p2, s_1, s_2)
+print 'test datum shift (requires cs2cs proj4 command line tool)'
+x1,y1,z1 = tuple( [float(x) for x in commands.getoutput(cstr).split()] )
+x2,y2 = transform(p1, p2, s_1, s_2)
+print 'is',x1,y1,'=',x2,y2,'?'
+print '%12.3f,%12.3f' % (x1,y1) == '%12.3f,%12.3f' % (x2,y2)
