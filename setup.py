ae7060d4508e192cec37eb349253f040fd87d482
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2011-03-04 23:35:30 +0000

    update version number to 1.8.9
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@221 ac951b51-2923-0410-bde8-533199751630

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ datafiles = [os.path.join('data',os.path.basename(f)) for f in datafiles]
 package_data = {'pyproj':datafiles}
 
 setup(name = "pyproj",
-  version = "1.8.8",
+  version = "1.8.9",
   description = "Python interface to PROJ.4 library",
   long_description  = """
 Performs cartographic transformations between geographic (lat/lon)
