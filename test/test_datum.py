5cc4efc29b8fe4b6e7cd1b942c1eed61bd360686
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2011-12-15 22:17:54 +0000

    make python 3 compatible
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@249 ac951b51-2923-0410-bde8-533199751630

--- test/test_datum.py
+++ test/test_datum.py
@@ -1,4 +1,4 @@
-import commands
+import subprocess, sys
 from pyproj import Proj, transform
 p1 = Proj(proj='latlong',datum='WGS84')
 s_1 = -111.5
@@ -6,8 +6,11 @@ s_2 = 45.25919444444
 p2 = Proj(proj="utm",zone=10,datum='NAD27')
 cstr='echo "-111.5 45.25919444444" | cs2cs +proj=latlong +datum=WGS84 +to '+\
      '+proj=utm +zone=10 +datum=NAD27 -f "%.12f"'
-print 'test datum shift (requires cs2cs proj4 command line tool)'
-x1,y1,z1 = tuple( [float(x) for x in commands.getoutput(cstr).split()] )
+sys.stdout.write('test datum shift (requires cs2cs proj4 command line tool)\n')
+p = subprocess.Popen(cstr, shell=True, stdout=subprocess.PIPE,\
+        stderr=subprocess.STDOUT)
+pout = p.stdout.readlines()[0].decode('ascii')
+x1,y1,z1 = tuple( [float(x) for x in pout.split()] )
 x2,y2 = transform(p1, p2, s_1, s_2)
-print 'is',(x1,y1),'=',(x2,y2),'?'
-print "%12.3f %12.3f" % (x1,y1) == '%12.3f %12.3f' % (x2,y2)
+sys.stdout.write('is (%s,%s) = (%s,%s)?\n' % (x1,y1,x2,y2))
+print("%12.3f %12.3f" % (x1,y1) == '%12.3f %12.3f' % (x2,y2))
