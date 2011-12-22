6c5012c7aac69af77bf15e44094c2c00b62ba507
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2011-12-22 22:37:28 +0000

    update
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@276 ac951b51-2923-0410-bde8-533199751630

--- test/geodtest.py
+++ test/geodtest.py
@@ -9,21 +9,21 @@ print lat1pt,lon1pt,lat2pt,lon2pt
 print 'inverse transform'
 print 'from proj.4 invgeod:'
 print commands.getoutput('echo "42d15\'N 71d07\'W 45d31\'N 123d41\'W" | geod +ellps=clrk66 -I -f "%.3f"')
-print 'from pyproj._Geod._inv:'
+print 'from pyproj.Geod.inv:'
 az12,az21,dist = g.inv(lon1pt,lat1pt,lon2pt,lat2pt)
 print "%7.3f %6.3f %12.3f" % (az12,az21,dist)
 print 'forward transform'
 print 'from proj.4 geod:'
 print commands.getoutput('echo "42d15\'N 71d07\'W -66d31\'50.141 4164192.708" | geod +ellps=clrk66 -f "%.3f"')
 endlon,endlat,backaz = g.fwd(lon1pt,lat1pt,az12,dist)
-print 'from pyproj._Geod._fwd:'
+print 'from pyproj.Geod.fwd:'
 print "%6.3f  %6.3f %13.3f" % (endlat,endlon,backaz)
 print 'intermediate points:'
 print 'from geod with +lat_1,+lon_1,+lat_2,+lon_2,+n_S:'
 points = '+lon_1=%s +lat_1=%s +lon_2=%s +lat_2=%s' % (lon1pt,lat1pt,lon2pt,lat2pt,)
 print points
 print commands.getoutput('geod +ellps=clrk66 -f "%.3f" +n_S=5 '+points)
-print 'from pyproj._Geod._npts:'
+print 'from pyproj.Geod.npts:'
 npts = 4
 lonlats = g.npts(lon1pt,lat1pt,lon2pt,lat2pt,npts)
 lonprev = lon1pt
@@ -36,25 +36,6 @@ for lon, lat in lonlats:
     latprev = lat; lonprev = lon
 az12,az21,dist = g.inv(lonprev,latprev,lon2pt,lat2pt)
 print '%6.3f  %7.3f %11.3f' % (lat2pt, lon2pt, dist)
-
-# should raise exception (antipodal point)
-try:
-    print 'testing antipodal point, should raise exception ...'
-    az12,az21,dist = g.inv(0.,90.,0.,-90.,0.)
-except ValueError:
-    print 'OK'
-else:
-    print 'not OK, no exception raised!'
-
-# should raise exception (equatorial arc)
-try:
-    print 'testing equatorial arc, should raise execption ...'
-    az12,az21,dist = g.inv(180.,0.0,200.,0.)
-    endlon,endlat,backaz = g.fwd(180.,0.0,az12,0.)
-except ValueError:
-    print 'OK'
-else:
-    print 'not OK, no exception raised!'
 
 # specify the lat/lons of some cities.
 boston_lat = 42.+(15./60.); boston_lon = -71.-(7./60.)
@@ -84,6 +65,6 @@ g3 = Geod('+ellps=clrk66') # proj4 style init string
 print 'inverse transform'
 print 'from proj.4 invgeod:'
 print commands.getoutput('echo "42d15\'N 71d07\'W 45d31\'N 123d41\'W" | geod +ellps=clrk66 -I -f "%.3f"')
-print 'from pyproj._Geod._inv:'
+print 'from pyproj.Geod.inv:'
 az12,az21,dist = g3.inv(lon1pt,lat1pt,lon2pt,lat2pt)
 print "%7.3f %6.3f %12.3f" % (az12,az21,dist)
