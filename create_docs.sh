0a2051609b8000c15e4de135818d8b2f90a67575
whitaker.jeffrey <whitaker.jeffrey@ac951b51-2923-0410-bde8-533199751630>
2012-12-28 17:06:31 +0000

    update docs for version 1.9.3
    git-svn-id: https://pyproj.googlecode.com/svn/trunk@321 ac951b51-2923-0410-bde8-533199751630

--- create_docs.sh
+++ create_docs.sh
@@ -1,2 +1,2 @@
 # svn propset svn:mime-type text/html docs/*html
-epydoc -v --exclude=_geod --exclude=_proj --no-frames --no-private --introspect-only -o docs pyproj
+epydoc -v --exclude=_geod --exclude=datadir --exclude=_proj --no-frames --no-private --introspect-only -o docs pyproj
