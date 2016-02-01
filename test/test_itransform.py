6cd84265eee4da229aced06ce08ab55d5b840a9d
georgeouzou <geothrock@gmail.com>
2016-02-01 17:49:52 +0200

    Fix itransform's python 2.4 compatibility issues and test_itransform python 3 comp. issues

--- test/test_itransform.py
+++ test/test_itransform.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, with_statement
+
 from itertools import count
 import random, os, math
 
@@ -7,25 +9,25 @@ import numpy as np
 p1 = Proj(init='epsg:4326')
 p2 = Proj(init='epsg:2100')
 
-print 'List with tuples'
+print ('List with tuples')
 pts = [(23, 38), (24, 38), (24, 39)]
 
 x2, y2 = transform(p1, p2, *zip(*pts)) # pyproj.transform
 x1y1 = itransform(p1, p2, pts)
 
 for xiyi , xj, yj in zip(x1y1, x2, y2):
-	print xiyi , ' == ', (xj, yj)
+	print (xiyi , ' == ', (xj, yj))
 
-print '2d numpy array of coordinates'
+print ('2d numpy array of coordinates')
 pts = np.array([(23, 38, 1), (24, 38, 0), (24, 39, 2)])
 
 x2, y2, z2 = transform(p1, p2, *zip(*pts)) # pyproj.transform
 x1y1z1 = itransform(p1, p2, pts)
 
 for xiyizi, xj, yj, zj in zip(x1y1z1, x2, y2, z2):
-	print xiyizi , ' == ', (xj, yj, zj)
+	print (xiyizi , ' == ', (xj, yj, zj))
 
-print 'Read and write a file'
+print ('Read and write a file')
 with open(os.path.join(os.path.dirname(__file__), 'itransform_coords.in.txt'),'r') as inf:
 	
 	coords_strings = (line.split() for line in inf)
