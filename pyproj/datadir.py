b379f7ccc0a87cadda86fdc2087896ea15054fef
snowman2 <alansnow21@gmail.com>
2019-02-16 13:12:44 -0600

    rename PROJ_DATA env var to PROJ_LIB

--- pyproj/datadir.py
+++ pyproj/datadir.py
@@ -3,13 +3,13 @@ Set the datadir path to the local data directory
 """
 import os
 
-if "PROJ_DATA" not in os.environ:
+if "PROJ_LIB" not in os.environ:
     base_dir = os.path.dirname(os.path.abspath(__file__))
     pyproj_datadir = os.path.join(base_dir, "proj_dir", "share", "proj")
     if not os.path.exists(pyproj_datadir):
         pyproj_datadir = os.path.join(base_dir, "proj_dir", "share")
     if not os.path.exists(pyproj_datadir):
-        raise RuntimeError("PROJ_DATA directory not found. Please set PROJ_DATA.")
-    os.environ["PROJ_DATA"] = pyproj_datadir
+        raise RuntimeError("PROJ_LIB directory not found. Please set PROJ_LIB.")
+    os.environ["PROJ_LIB"] = pyproj_datadir
 else:
-    pyproj_datadir = os.environ["PROJ_DATA"]
+    pyproj_datadir = os.environ["PROJ_LIB"]
