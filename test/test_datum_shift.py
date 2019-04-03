8a0ed54122c7ae16d156446dab82d74d4e008351
snowman2 <alansnow21@gmail.com>
2019-04-02 19:43:04 -0500

    updated gauss datum shift test EPSG from 26592 (deprecated) to 3004

--- test/test_datum_shift.py
+++ test/test_datum_shift.py
@@ -30,7 +30,7 @@ UTM_z = WGS84_z = 52.8  # Ellipsoidical height in meters
 WGS84_PROJ = Proj(proj="latlong", datum="WGS84")
 UTM_33_PROJ = Proj(proj="utm", zone="33")
 GAUSSSB_PROJ = Proj(
-    init="epsg:26592", towgs84="-122.74,-34.27,-22.83,-1.884,-3.400,-3.030,-15.62"
+    init="epsg:3004", towgs84="-122.74,-34.27,-22.83,-1.884,-3.400,-3.030,-15.62"
 )
 print("proj4 library version = ", WGS84_PROJ.proj_version)
 
@@ -47,21 +47,13 @@ def test_shift_utm33_to_wgs84():
     assert_almost_equal((back_lon, back_lat, back_z), (WGS84_lon, WGS84_lat, WGS84_z))
 
 
-@pytest.mark.xfail
 def test_shift_wgs84_to_gaussb_no_ellisoidal_height():
     xgb, ygb, zgb = transform(WGS84_PROJ, GAUSSSB_PROJ, WGS84_lon, WGS84_lat, 0)
-    assert_almost_equal((xgb, ygb, zgb), (GB_x, GB_y, 0))
-    # 1.9.6
-    # (2423346.995008026, 5055619.899023423, 0.16683581471443176)
-    # 2.1.3
-    # (1453270.7157727296, 5146884.706783104, 0.0)
+    assert_almost_equal((xgb, ygb, zgb), (GB_x, 5055619.899, 0), decimal=2)
 
 
-@pytest.mark.xfail
 def test_shift_gaussb_to_wgs84_no_ellisoidal_height():
     back_lon, back_lat, back_z = transform(GAUSSSB_PROJ, WGS84_PROJ, GB_x, GB_y, 0)
-    assert_almost_equal((back_lon, back_lat, back_z), (WGS84_lon, WGS84_lat, 0))
-    # 1.9.6
-    # (13.759554641526984, 45.64718834776721, -0.16560534108430147)
-    # 2.1.3
-    # (26.21239770046797, 45.64699807457936, 0.0)
+    assert_almost_equal(
+        (back_lon, back_lat, back_z), (WGS84_lon, WGS84_lat, 0), decimal=3
+    )
