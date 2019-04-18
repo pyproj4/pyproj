cb81eeac5d2819d90b4ef2672ab2eb9553342300
snowman2 <alansnow21@gmail.com>
2019-04-17 21:04:30 -0500

    fixed initialization from non DerivedGeographicCRS (fixes #270)

--- pyproj/crs.py
+++ pyproj/crs.py
@@ -41,18 +41,23 @@ from pyproj.geod import Geod
 def _dict2string(projparams):
     # convert a dict to a proj4 string.
     pjargs = []
+    proj_inserted = False
     for key, value in projparams.items():
         # the towgs84 as list
         if isinstance(value, (list, tuple)):
             value = ",".join([str(val) for val in value])
         # issue 183 (+ no_rot)
         if value is None or value is True:
-            pjargs.append("+" + key + " ")
+            pjargs.append("+{key}".format(key=key))
         elif value is False:
             pass
+        # make sure string starts with proj or init
+        elif not proj_inserted and key in ("init", "proj"):
+            pjargs.insert(0, "+{key}={value}".format(key=key, value=value))
+            proj_inserted = True
         else:
-            pjargs.append("+" + key + "=" + str(value) + " ")
-    return "".join(pjargs)
+            pjargs.append("+{key}={value}".format(key=key, value=value))
+    return " ".join(pjargs)
 
 
 class CRS(_CRS):
---
+++ test/test_proj.py
@@ -0,0 +1,16 @@
+from pyproj import Proj
+
+
+def test_initialize_proj_crs_no_proj4():
+    proj = Proj(
+        {
+            "a": 6371229.0,
+            "b": 6371229.0,
+            "lon_0": -10.0,
+            "o_lat_p": 30.0,
+            "o_lon_p": 0.0,
+            "o_proj": "longlat",
+            "proj": "ob_tran",
+        }
+    )
+    assert proj.srs.startswith("+proj=ob_tran")
