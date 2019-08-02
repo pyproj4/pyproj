12e2a1860f391953dc50f0a1caea555746ebc228
Alan D. Snow <alansnow21@gmail.com>
2019-08-01 19:38:52 -0500

    Add allcontributors setup (#379)

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ def get_long_description():
     """
     Get the long description for the file.
     """
-    with open("README.md") as ld_file:
+    with open("README.md", encoding="utf-8") as ld_file:
         return ld_file.read()
 
 
