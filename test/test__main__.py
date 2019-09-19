8bda08cd0f828e1ce6f1f0c2f66389b86880458e
Alan D. Snow <alansnow21@gmail.com>
2019-09-19 07:42:40 -0500

    update to use sys.executable for test in main (#454)

--- test/test__main__.py
+++ test/test__main__.py
@@ -1,10 +1,13 @@
 import subprocess
+import sys
 
 import pytest
 
 
 def test_main():
-    output = subprocess.check_output(["python", "-m", "pyproj"]).decode("utf-8")
+    output = subprocess.check_output(
+        [sys.executable, "-m", "pyproj"], stderr=subprocess.STDOUT
+    ).decode("utf-8")
     assert "pyproj version:" in output
     assert "PROJ version:" in output
     assert "-v, --verbose  Show verbose debugging version information." in output
@@ -12,7 +15,9 @@ def test_main():
 
 @pytest.mark.parametrize("option", ["-v", "--verbose"])
 def test_main__verbose(option):
-    output = subprocess.check_output(["python", "-m", "pyproj", option]).decode("utf-8")
+    output = subprocess.check_output(
+        [sys.executable, "-m", "pyproj", option], stderr=subprocess.STDOUT
+    ).decode("utf-8")
     assert "pyproj:" in output
     assert "PROJ:" in output
     assert "data dir" in output
