79983df83545d4ac2f25465946767bc713a47651
Alan D. Snow <alansnow21@gmail.com>
2019-09-20 07:31:22 -0500

    added tmp_chdir context manger for tests in main to prevent conflicts (#455)

--- test/test__main__.py
+++ test/test__main__.py
@@ -1,23 +1,42 @@
+import contextlib
+import os
 import subprocess
 import sys
 
 import pytest
 
 
-def test_main():
-    output = subprocess.check_output(
-        [sys.executable, "-m", "pyproj"], stderr=subprocess.STDOUT
-    ).decode("utf-8")
+@contextlib.contextmanager
+def tmp_chdir(new_dir):
+    """
+    This temporarily changes directories when running the tests.
+    Useful for when testing wheels in the pyproj directory
+    when pyproj has not been build and prevents conflicts.
+    """
+    curdir = os.getcwd()
+    try:
+        os.chdir(new_dir)
+        yield
+    finally:
+        os.chdir(curdir)
+
+
+def test_main(tmpdir):
+    with tmp_chdir(str(tmpdir)):
+        output = subprocess.check_output(
+            [sys.executable, "-m", "pyproj"], stderr=subprocess.STDOUT
+        ).decode("utf-8")
     assert "pyproj version:" in output
     assert "PROJ version:" in output
     assert "-v, --verbose  Show verbose debugging version information." in output
 
 
 @pytest.mark.parametrize("option", ["-v", "--verbose"])
-def test_main__verbose(option):
-    output = subprocess.check_output(
-        [sys.executable, "-m", "pyproj", option], stderr=subprocess.STDOUT
-    ).decode("utf-8")
+def test_main__verbose(option, tmpdir):
+    with tmp_chdir(str(tmpdir)):
+        output = subprocess.check_output(
+            [sys.executable, "-m", "pyproj", option], stderr=subprocess.STDOUT
+        ).decode("utf-8")
     assert "pyproj:" in output
     assert "PROJ:" in output
     assert "data dir" in output
