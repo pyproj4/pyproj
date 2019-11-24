83a75b08dcf30908510679d5b95a5a195f0f930d
snowman2 <alansnow21@gmail.com>
2019-11-23 19:54:36 -0600

    DOC: activated sphinx.ext.intersphinx for docs (geopandas/geopandas/pull/1101)

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,12 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
