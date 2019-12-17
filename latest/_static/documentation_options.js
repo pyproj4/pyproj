7832bb2486a2cacbaa53d49a1dcf86986f804cee
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-12-17 19:38:33 +0000

    Update docs after building Travis build 1231 of pyproj4/pyproj
    The docs were built from the branch 'master' against the commit
    2467da423aa371b755b561a7e8b7bdfafd5ab91a.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pyproj4/pyproj/jobs/626360044.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --build-tags --built-docs docs/_build/html latest

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '2.4.2.post1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
