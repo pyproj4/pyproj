f4bc0a2ce4362cd2263539be87b3d5af67c98900
snowman2 <alansnow21@gmail.com>
2019-12-30 19:35:15 -0600

    add link to PROJ issue for datum test

--- test/test_datum.py
+++ test/test_datum.py
@@ -16,4 +16,5 @@ def test_datum(proj_class, aoi_data_directory):
     if LooseVersion(proj_version_str) < LooseVersion("6.3.0"):
         assert_almost_equal((x2, y2), (1402291.0833290431, 5076289.591846835))
     else:
+        # https://github.com/OSGeo/PROJ/issues/1808
         assert_almost_equal((x2, y2), (1402285.9829252, 5076292.4212746))
