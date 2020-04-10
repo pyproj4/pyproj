e1e644890c8427bb4e09abaf7c6000557097f25c
snowman2 <alansnow21@gmail.com>
2020-04-09 19:03:23 -0500

    datum tests precision to 2 decimal places

--- test/test_datum.py
+++ test/test_datum.py
@@ -15,10 +15,12 @@ def test_datum(proj_class):
     p2 = proj_class(proj="utm", zone=10, datum="NAD27")
     x2, y2 = transform(p1, p2, s_1, s_2)
     if LooseVersion(proj_version_str) < LooseVersion("6.3.0"):
-        assert_almost_equal((x2, y2), (1402291.0833290431, 5076289.591846835))
+        assert_almost_equal(
+            (x2, y2), (1402291.0833290431, 5076289.591846835), decimal=2
+        )
     else:
         if grids_available("ca_nrc_ntv2_0.tif"):
-            assert_almost_equal((x2, y2), (1402286.3333203, 5076292.2955777))
+            assert_almost_equal((x2, y2), (1402286.3333203, 5076292.2955777), decimal=2)
         else:
             # https://github.com/OSGeo/PROJ/issues/1808
-            assert_almost_equal((x2, y2), (1402285.9829252, 5076292.4212746))
+            assert_almost_equal((x2, y2), (1402285.9829252, 5076292.4212746), decimal=2)
