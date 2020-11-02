77759e4e2049db1cdb2550d2b9361ae6f6400aa3
snowman2 <alansnow21@gmail.com>
2020-11-02 08:05:38 -0600

    pyproj CLI

--- docs/conf.py
+++ docs/conf.py
@@ -140,7 +140,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [("cli", "pyproj", "pyproj Documentation", [author], 1)]
+man_pages = [("cli", "pyproj", "pyproj CLI", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------
