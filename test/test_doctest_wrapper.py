baed8e133cf33a9a07364ba8194f2bd63816a404
snowman2 <alansnow21@gmail.com>
2020-11-02 19:00:15 -0600

    TST: Add marker for tests that require the network enabled

--- test/test_doctest_wrapper.py
+++ test/test_doctest_wrapper.py
@@ -12,9 +12,7 @@ from test.conftest import proj_network_env
 
 def test_doctests():
     """run the examples in the docstrings using the doctest module"""
-
-    with warnings.catch_warnings(), proj_network_env():
-        pyproj.network.set_network_enabled(active=True)
+    with warnings.catch_warnings():
         warnings.filterwarnings(
             "ignore",
             "You will likely lose important projection information when",
@@ -24,17 +22,8 @@ def test_doctests():
         failure_count_proj, test_count = doctest.testmod(pyproj.proj, verbose=True)
         failure_count_crs, test_count_crs = doctest.testmod(pyproj.crs, verbose=True)
         failure_count_geod, test_count_geod = doctest.testmod(pyproj.geod, verbose=True)
-        with pytest.warns(DeprecationWarning):
-            failure_count_transform, test_count_transform = doctest.testmod(
-                pyproj.transformer, verbose=True
-            )
 
-    failure_count = (
-        failure_count_proj
-        + failure_count_crs
-        + failure_count_geod
-        + failure_count_transform
-    )
+    failure_count = failure_count_proj + failure_count_crs + failure_count_geod
     expected_failure_count = 0
     try:
         import shapely  # noqa
@@ -46,3 +35,16 @@ def test_doctests():
     assert (
         failure_count == expected_failure_count
     ), f"{failure_count} of the doctests failed"
+
+
+@pytest.mark.network
+def test_doctests__network():
+    """run the examples in the docstrings using the doctest module
+    that require the network
+    """
+    with proj_network_env():
+        pyproj.network.set_network_enabled(active=True)
+        with pytest.warns(DeprecationWarning):
+            failure_count, _ = doctest.testmod(pyproj.transformer, verbose=True)
+
+    assert failure_count == 0, f"{failure_count} of the doctests failed"
