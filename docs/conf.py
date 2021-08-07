26c3acd39379af3ef1729cab7a1b36f972ffa9b8
Brendan Jurd <direvus@gmail.com>
2021-08-07 23:53:18 +1000

    Update logo to use Python colours in globe symbol.
    The ellipse uses the blue Python colour and the axis line uses the
    yellow.
    
    Also add a favicon that is the globe symbol on its own.

--- docs/conf.py
+++ docs/conf.py
@@ -97,6 +97,7 @@ todo_include_todos = False
 #
 html_theme = "sphinx_rtd_theme"
 html_logo = 'media/logo.png'
+html_favicon = 'media/icon.png'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
