746ba56a5e94b38a71020164f8699e44850ec5a0
Alan D. Snow <alansnow21@gmail.com>
2021-10-09 19:47:26 -0500

    CI: Use cmake to build PROJ (#979)

--- ci/proj-compile.sh
+++ ci/proj-compile.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 pushd .
-echo "Building PROJ $1 version from source..."
+echo "Building PROJ ($1) from source..."
+BUILD_PROJ_DIR=proj-${1:0:5}
+# Download PROJ
 if [[ $1 == "git" ]]; then
-  git clone https://github.com/OSGeo/PROJ.git proj-git
+  git clone https://github.com/OSGeo/PROJ.git ${BUILD_PROJ_DIR}
 else
-  curl https://download.osgeo.org/proj/proj-$1.tar.gz > "proj-${1:0:5}.tar.gz"
-  tar zxf "proj-${1:0:5}.tar.gz"
+  curl https://download.osgeo.org/proj/proj-$1.tar.gz > ${BUILD_PROJ_DIR}.tar.gz
+  tar zxf ${BUILD_PROJ_DIR}.tar.gz
+  rm ${BUILD_PROJ_DIR}.tar.gz
 fi
-cd "proj-${1:0:5}"
-# build using autotools
-sh autogen.sh
-./configure --prefix=$PROJ_DIR
-make
-make install
+cd ${BUILD_PROJ_DIR}
+mkdir build
+cd build
 # build using cmake
-#cmake . -DCMAKE_INSTALL_PREFIX=$PROJ_DIR
-#cmake --build .
-#make install
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$PROJ_DIR \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_IPO=ON \
+    -DBUILD_CCT:BOOL=OFF \
+    -DBUILD_CS2CS:BOOL=OFF \
+    -DBUILD_GEOD:BOOL=OFF \
+    -DBUILD_GIE:BOOL=OFF \
+    -DBUILD_GMOCK:BOOL=OFF \
+    -DBUILD_PROJINFO:BOOL=OFF \
+    -DBUILD_PROJSYNC:BOOL=OFF \
+    -DBUILD_TESTING:BOOL=OFF
+cmake --build . -j$(nproc)
+cmake --install .
+# cleanup
+cd ../..
+rm -rf ${BUILD_PROJ_DIR}
 popd
