356e45bd58a83a9aae7050a99ed0c41152048101
snowman2 <alansnow21@gmail.com>
2023-03-24 20:52:18 -0500

    MNT: version 3.5.0rc0

--- pyproj/__init__.py
+++ pyproj/__init__.py
@@ -66,7 +66,7 @@ from pyproj.transformer import (  # noqa: F401 pylint: disable=unused-import
     transform,
 )
 
-__version__ = "3.5.0.dev0"
+__version__ = "3.5.0rc0"
 __all__ = [
     "Proj",
     "Geod",
