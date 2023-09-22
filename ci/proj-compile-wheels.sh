27dc3f9537d430a26a7f5dd965c0cbc324350029
David Hoese <david.hoese@ssec.wisc.edu>
2023-09-22 10:55:16 -0500

    Use a recursive copy for PROJ cache

--- ci/proj-compile-wheels.sh
+++ ci/proj-compile-wheels.sh
@@ -294,12 +294,12 @@ function copy_cached_proj_to_build {
         return 1
     fi
 
-    cp ${PROJ_CACHE}/* ${PROJ_DIR}/
+    cp -r ${PROJ_CACHE}/* ${PROJ_DIR}/
 }
 
 function copy_build_proj_to_cache {
     mkdir -p ${PROJ_CACHE}
-    cp ${PROJ_DIR}/* ${PROJ_CACHE}/
+    cp -r ${PROJ_DIR}/* ${PROJ_CACHE}/
 }
 
 
